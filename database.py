64997d64d4d82eb317cae89ffcb3e2402a227a3d
Mahmoud Aboelsoud <mahmoud.aboelsoud.p@gmail.com>
2020-02-22 20:21:03 +0200

    Connect to the DB on Heroku

--- database.py
+++ database.py
@@ -6,8 +6,10 @@ from psycopg2.extras import DictCursor
 def connect_db():
     """Return a new connection to the DB"""
 
-    connection = psycopg2.connect(host="localhost", database="learning",
-                                  user="postgres", password="192168-Water",
+    connection = psycopg2.connect("""postgres://tmsiqmturzvdkn:7c8ad8a3a8f1ccec
+                                  f738241bf3dfd1047d502a52357aaebea3dcfa350b46b93c
+                                  @ec2-3-230-106-126.compute-1.amazonaws.com:5432
+                                  /d4es9ame7b1hs9""",
                                   cursor_factory=DictCursor)
 
     return connection
