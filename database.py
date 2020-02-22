3d8749c3435b61bdd781c91a624b18da72ff6c7e
Mahmoud Aboelsoud <mahmoud.aboelsoud.p@gmail.com>
2020-02-22 20:35:31 +0200

    Fix an error in the DB connection

--- database.py
+++ database.py
@@ -6,10 +6,10 @@ from psycopg2.extras import DictCursor
 def connect_db():
     """Return a new connection to the DB"""
 
-    connection = psycopg2.connect("""postgres://tmsiqmturzvdkn:7c8ad8a3a8f1ccec
-                                  f738241bf3dfd1047d502a52357aaebea3dcfa350b46b93c
-                                  @ec2-3-230-106-126.compute-1.amazonaws.com:5432
-                                  /d4es9ame7b1hs9""",
+    connection = psycopg2.connect(("postgres://tmsiqmturzvdkn:7c8ad8a3a8f1cce"
+                                   "cf738241bf3dfd1047d502a52357aaebea3dcfa350"
+                                   "b46b93c@ec2-3-230-106-126.compute-1."
+                                   "amazonaws.com:5432/d4es9ame7b1hs9"),
                                   cursor_factory=DictCursor)
 
     return connection
